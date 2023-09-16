c135464f6d8bcbae2d3c87a1976915a61eaf90e4
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-09-16 20:48:22 +0200

    Finish client

--- client.py
+++ client.py
@@ -1,4 +1,12 @@
 import socket
 
+MAX_SIZE_BYTES = 65535
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print(f'The OS assigned the address {s.getsockname()} to me')
+message = input('Input lowercase sentence: ')
+data = message.encode('ascii')
+s.sendto(data, ('127.0.0.1', 3000))
+print('The OS assigned the address {} to me'.format(s.getsockname()))
+data, address = s.recvfrom(MAX_SIZE_BYTES)
+text = data.decode('ascii')
+print(f'The server {address} replied with {text!r}')
